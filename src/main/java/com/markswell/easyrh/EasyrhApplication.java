e7f68076f5c0615bb5403835f172bf0526ce6d18
markswell <markswellmenezes@outlook.com>
2020-06-28 13:25:28 -0300

    Versão final.

--- src/main/java/com/markswell/easyrh/EasyrhApplication.java
+++ src/main/java/com/markswell/easyrh/EasyrhApplication.java
@@ -2,13 +2,20 @@ package com.markswell.easyrh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EasyrhApplication {
+public class EasyrhApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		System.setProperty("server.servlet.context-path", "/api");
 		SpringApplication.run(EasyrhApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EasyrhApplication.class);
 	}
 
 }
